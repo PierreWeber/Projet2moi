06cb49b7ef867c49f86a31b1b53d6c1f03aaf75f
pierre weber <102355037+PierreWeber@users.noreply.github.com>
2022-07-20 14:27:27 +0200

    Documentation & Sauvegarde

--- Script_Exemples_parametre.sh
+++ Script_Exemples_parametre.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## affichage des parametres comme exemple : 
+## affichage des parametres comme exemple  
 
 echo $0 	&& echo "le nom du script (du fichier)"
 echo $# 	&& echo "le nombre de paramètres du script"
@@ -16,16 +16,16 @@ $1  && echo "le 1er parametre mis apres la commande"
 echo $? && echo "le code de retour de la dernière commande exécutée"
 
 #!/bin/bash
-# début du script de bienvenue 
+### début du script de bienvenue 
 echo "Hello $USER"
 echo "You are currently on $HOST"
 echo "The content of your homedir:"
-/bin/ls					## accede au chemin directement sans condition ou boucle ni variable 
-NBFILES=$(ls | wc -l) 			# affectation de la variable NBFILES avec une commande sur un pipe 
+/bin/ls					                                ## accede au chemin directement sans condition ou boucle ni variable 
+NBFILES=$(ls | wc -l) 			                    # affectation de la variable NBFILES avec une commande sur un pipe 
 echo "Total: $NBFILES elements"
 
 #!/bin/bash
-host=`cat /etc/hostname`  		## lit le fichier hostname dans le chemin completement 
+host=`cat /etc/hostname`  		                  ## lit le fichier hostname dans le chemin completement 
 echo -n "hello $USER, you are logged on $host and current date and time are :"
 date 
 
@@ -35,7 +35,7 @@ echo -n "Hello, $#"
 
 #!/bin/bash
 echo  $0
-for file in * 		## lit le dossier en cour  
+for file in * 		                              ## lit le dossier en cour  
 do 
 cat  $file 
 done
